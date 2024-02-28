484a5178b407dfd76149b4f06899d17976e061a0
Amgad226 <amgad.wr.1@gmail.com>
2024-02-28 04:09:20 +0300

    ci

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const users = {};
 const socketToRoom = {};
 app.get('/', (req, res) => {
     return res.json({
-        message: "mesh server code"
+        message: "mesh server code new"
     });
 }
 );
