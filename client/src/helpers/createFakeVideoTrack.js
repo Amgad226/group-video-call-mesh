12b391ff9ac5ef6c5ab2a5e8eb253216ff6efd83
Ayham Hammami <ayhmhamame14@gmail.com>
2024-03-21 15:04:20 +0300

    fix the voice issue when no camera exist and using the canvas track

--- client/src/helpers/createFakeVideoTrack.js
+++ client/src/helpers/createFakeVideoTrack.js
@@ -1,14 +1,26 @@
+import img from "../assets/test.png";
+
 export function createFakeVideoTrack() {
   const canvas = document.createElement("canvas");
   canvas.width = 640; // Set desired video width
   canvas.height = 480; // Set desired video height
 
+  // const image = new Image();
+  // image.src = img; // Replace with the path to your image
+
+  // image.addEventListener("load", () => {
+  //   context.drawImage(image, 0, 0, canvas.width, canvas.height);
+  // });
+
   const context = canvas.getContext("2d");
-  context.fillStyle = "black";
+  // context.fillStyle = "red";
   context.fillRect(0, 0, canvas.width, canvas.height);
+  setInterval(() => context.clearRect(0, 0, 1, 1), 500);
+  context.clearRect(0, 0, 1, 1);
 
   const fakeStream = canvas.captureStream();
   const videoTrack = fakeStream.getVideoTracks()[0];
   videoTrack.enabled = false;
+
   return videoTrack;
 }
