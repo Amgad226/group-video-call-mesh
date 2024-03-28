14ca9146aed095ed327254d061252769352d770e
Ayham Hammami <ayhmhamame14@gmail.com>
2024-03-28 13:44:03 +0300

    move the check peers connection state to new helper

---
+++ client/src/helpers/chcekAllPeersConnectionState.js
@@ -0,0 +1,92 @@
+const CONNECTION_CLOSED_THRESHOLD = 5; // Define the threshold value
+const CONNECTION_RETRY_THRESHOLD = 15; // Define the threshold value
+
+export const checkAllPeersConnectionState = ({
+  counters,
+  peers,
+  setConnectionFailedReason,
+  setFireCheckState,
+  setLoading,
+}) => {
+  const recursiveFunction = () => {
+    let allConnected = true;
+    let anyClosedConnections = false;
+    let anyFailedConnections = false;
+    let anyRetryConnectionsExceededThreshold = false;
+
+    const updatedPeers = peers.map((peerObj) => {
+      const { peerID, peer } = peerObj;
+
+      console.warn(
+        peer.connectionState,
+        peerID,
+        counters[peerID]?.connectionClosedCount,
+        counters[peerID]?.connectionRetryCount
+      );
+
+      // Retrieve the counters for the current peer from the counters object
+      const { connectionClosedCount = 0, connectionRetryCount = 0 } =
+        counters[peerID] || {};
+
+      if (peer.connectionState === "connected") {
+        return peerObj;
+      } else if (peer.connectionState === "closed") {
+        if (connectionClosedCount < CONNECTION_CLOSED_THRESHOLD) {
+          // Update the counter for the current peer
+          counters[peerID] = {
+            connectionClosedCount: connectionClosedCount + 1,
+            connectionRetryCount,
+          };
+          return { peerID, peer };
+        } else {
+          anyClosedConnections = true;
+          return null;
+        }
+      } else if (
+        peer.connectionState === "failed" ||
+        peer.connectionState === "disconnected"
+      ) {
+        anyFailedConnections = true;
+        return null;
+      } else {
+        if (connectionRetryCount < CONNECTION_RETRY_THRESHOLD) {
+          // Update the counter for the current peer
+          counters[peerID] = {
+            connectionClosedCount,
+            connectionRetryCount: connectionRetryCount + 1,
+          };
+          return { peerID, peer };
+        } else {
+          anyRetryConnectionsExceededThreshold = true;
+          return null;
+        }
+      }
+    });
+
+    const filteredPeers = updatedPeers.filter(Boolean);
+    if (filteredPeers.length === 0) {
+      setConnectionFailedReason(
+        anyClosedConnections
+          ? "closed"
+          : anyRetryConnectionsExceededThreshold
+          ? "timeout"
+          : "failed"
+      );
+      setFireCheckState(false);
+      setLoading(false);
+      return;
+    }
+
+    allConnected = filteredPeers.every(
+      (peerObj) => peerObj.peer.connectionState === "connected"
+    );
+
+    if (!allConnected) {
+      setTimeout(recursiveFunction, 1000); // Recall the function after a short delay
+    } else {
+      setLoading(false);
+      setFireCheckState(false);
+    }
+  };
+  recursiveFunction();
+};
--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -16,6 +16,7 @@ import { getAvaliableUserMedia } from "../helpers/getAvaliableUserMedia";
 import { getUserAgent } from "../helpers/getUserAgent";
 import styles from "./styles.module.scss";
 import { Col, Modal, Row, Spin } from "antd";
+import { checkAllPeersConnectionState } from "../helpers/chcekAllPeersConnectionState";
 
 const Room = () => {
   const socketRef = useRef();
@@ -444,95 +445,16 @@ const Room = () => {
 
   //By Chat gpt
   useEffect(() => {
-    if (loading && fireCheckState) {
-      const CONNECTION_CLOSED_THRESHOLD = 5; // Define the threshold value
-      const CONNECTION_RETRY_THRESHOLD = 15; // Define the threshold value
-
+    if (loading && fireCheckState && peers) {
       // Create an object to store counters for each peer
-      let counters = {};
 
-      const checkConnectionStates = () => {
-        let allConnected = true;
-        let anyClosedConnections = false;
-        let anyFailedConnections = false;
-        let anyRetryConnectionsExceededThreshold = false;
-
-        const updatedPeers = peers.map((peerObj) => {
-          const { peerID, peer } = peerObj;
-
-          console.warn(
-            peer.connectionState,
-            peerID,
-            counters[peerID]?.connectionClosedCount,
-            counters[peerID]?.connectionRetryCount
-          );
-
-          // Retrieve the counters for the current peer from the counters object
-          const { connectionClosedCount = 0, connectionRetryCount = 0 } =
-            counters[peerID] || {};
-
-          if (peer.connectionState === "connected") {
-            return peerObj;
-          } else if (peer.connectionState === "closed") {
-            if (connectionClosedCount < CONNECTION_CLOSED_THRESHOLD) {
-              // Update the counter for the current peer
-              counters[peerID] = {
-                connectionClosedCount: connectionClosedCount + 1,
-                connectionRetryCount,
-              };
-              return { peerID, peer };
-            } else {
-              anyClosedConnections = true;
-              return null;
-            }
-          } else if (
-            peer.connectionState === "failed" ||
-            peer.connectionState === "disconnected"
-          ) {
-            anyFailedConnections = true;
-            return null;
-          } else {
-            if (connectionRetryCount < CONNECTION_RETRY_THRESHOLD) {
-              // Update the counter for the current peer
-              counters[peerID] = {
-                connectionClosedCount,
-                connectionRetryCount: connectionRetryCount + 1,
-              };
-              return { peerID, peer };
-            } else {
-              anyRetryConnectionsExceededThreshold = true;
-              return null;
-            }
-          }
-        });
-
-        const filteredPeers = updatedPeers.filter(Boolean);
-        if (filteredPeers.length === 0) {
-          setConnectionFailedReason(
-            anyClosedConnections
-              ? "closed"
-              : anyRetryConnectionsExceededThreshold
-              ? "timeout"
-              : "failed"
-          );
-          setFireCheckState(false);
-          setLoading(false);
-          return;
-        }
-
-        allConnected = filteredPeers.every(
-          (peerObj) => peerObj.peer.connectionState === "connected"
-        );
-
-        if (!allConnected) {
-          setTimeout(checkConnectionStates, 1000); // Recall the function after a short delay
-        } else {
-          setLoading(false);
-          setFireCheckState(false);
-        }
-      };
-
-      checkConnectionStates(); // Call the function initially
+      checkAllPeersConnectionState({
+        counters: {},
+        peers,
+        setConnectionFailedReason,
+        setFireCheckState,
+        setLoading,
+      }); // Call the function initially
     }
   }, [fireCheckState, peers]);
 
